53b12f5821ba48838c312149ba77cc45ba0ce102
Haelle <pihaelle@gmail.com>
2016-12-06 17:33:47 +0100

    minify all js

--- gruntfile.js
+++ gruntfile.js
@@ -25,6 +25,15 @@ module.exports = function(grunt) {
         concat: {
             js: {
                 src: [
+                    './app/bower_components/jquery/dist/jquery.js',
+                    './app/bower_components/angular/angular.js',
+                    './app/bower_components/angular-animate/angular-animate.js',
+                    './app/bower_components/angular-resource/angular-resource.js',
+                    './app/bower_components/angular-route/angular-route.js',
+                    './app/bower_components/angular-sanitize/angular-sanitize.js',
+                    './app/bower_components/angular-translate/angular-translate.js',
+                    './app/bower_components/moment/moment.js',
+                    './app/bower_components/angular-moment/angular-moment.js',
                     './app/scripts/app.annotated.js',
                     './app/scripts/config.annotated.js',
                     './app/**/*.module.annotated.js',
