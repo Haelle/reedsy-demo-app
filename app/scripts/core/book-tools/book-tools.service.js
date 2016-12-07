a730bf6bf382a567d99de0fb9d2d70e7c64d4dbd
Haelle <pihaelle@gmail.com>
2016-12-07 13:01:45 +0100

    book-tools: advices provider

--- app/scripts/core/book-tools/book-tools.service.js
+++ app/scripts/core/book-tools/book-tools.service.js
@@ -1,3 +1,4 @@
+// Non optimized methods should be REST services like book.service from multiple provided json
 angular.module('core.bookTools').factory('BookTools', ['$http',
     function ($http) {
         var getCategories = function () {
@@ -41,9 +42,52 @@ angular.module('core.bookTools').factory('BookTools', ['$http',
             });
         };
 
+        // this function sucks... should be a REST service
         var getSelectedBooks = function(bookId){
             return $http.get('books/book.json').then(function (response) {
-                return response.data.slice(0, 3);
+                var books = response.data;
+
+                // find the book
+                var book = {};
+                for (var i = 0; i < books.length; i++){
+                    if (books[i].id == bookId) {
+                        book = books[i];
+                        break;
+                    }
+                }
+
+                // init counter and resultset
+                var selectedBooks = [];
+                var count = 0;
+                // find first 3 books with the same genre
+                for (var j = 0; j < books.length; j++) {
+                    if (books[j].genre.name == book.genre.name && books[j].id != book.id) {
+                        selectedBooks.push(books[j]);
+                        count++;
+                    }
+
+                    if (count == 3) break;
+                }
+
+                // if not 3 get the first 3
+                if (count < 3) {
+                    for (var k = 0; k < books.length; k++) {
+                        var currBookId = books[k].id;
+                        var inArray = selectedBooks.inArray(function(e){
+                            return e.id == currBookId;
+                        });
+
+                        if (!inArray && books[k].id != book.id) {
+                            selectedBooks.push(books[k]);
+                            count++;
+                        }
+
+                        if (count == 3) break;
+                    }
+                }
+
+                console.log(selectedBooks);
+                return selectedBooks;
             });
         };
 
